20a1b42a7490e1e5cc8611a119d8c9c2d0018d12
SergioVF89 <fonnegrasergio@gmail.com>
2026-04-09 18:26:24 -0500

    Created ISponsorService

---
+++ SportsLeague.Domain/Interfaces/Services/ISponsorService.cs
@@ -0,0 +1,18 @@
+﻿using SportsLeague.Domain.Entities;
+
+namespace SportsLeague.Domain.Interfaces.Services;
+
+public interface ISponsorService
+{
+    // CRUD básico
+    Task<IEnumerable<Sponsor>> GetAllAsync();
+    Task<Sponsor?> GetByIdAsync(int id);
+    Task<Sponsor> CreateAsync(Sponsor sponsor);
+    Task UpdateAsync(int id, Sponsor sponsor);
+    Task DeleteAsync(int id);
+
+    // Métodos de vinculación
+    Task<TournamentSponsor> LinkToTournamentAsync(int sponsorId, int tournamentId, decimal contractAmount);
+    Task UnlinkFromTournamentAsync(int sponsorId, int tournamentId);
+    Task<IEnumerable<Tournament>> GetTournamentsBySponsorAsync(int sponsorId);
+}
